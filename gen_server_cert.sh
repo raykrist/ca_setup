54270fda732e558dc8ebc92a8ca13fe4c51a3d4d
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-07-11 16:09:12 +0200

    more

---
+++ gen_server_cert.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+host=$1
+cnf="cnf/${host}.cnf"
+
+if [ -f "cnf/${host}.cnf" ]; then
+  cnf="cnf/${host}.cnf"
+else
+  cnf="intermediate/openssl.cnf"
+fi
+
+echo "Using ${cnf} as config file"
+
+openssl genrsa -out intermediate/private/${host}.key.pem 2048
+
+
+openssl req -config ${cnf} -key intermediate/private/${host}.key.pem -new -sha256 -out intermediate/csr/${host}.csr.pem
+
+openssl ca -config intermediate/openssl.cnf -extensions v3_req \
+  -days 375 -notext -md sha256 -in intermediate/csr/${host}.csr.pem \
+  -out intermediate/certs/${host}.cert.pem
