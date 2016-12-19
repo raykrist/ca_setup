74e6a1c3d9d020574a4c3edc329144b6bbd084ab
Raymond Kristiansen <raymond@nexusweb.no>
2016-12-19 12:20:55 +0100

    Updated cert generation script to run with batch

--- gen_server_cert.sh
+++ gen_server_cert.sh
@@ -11,15 +11,27 @@ cnf="cnf/${host}.cnf"
 if [ -f "cnf/${host}.cnf" ]; then
   cnf="cnf/${host}.cnf"
 else
-  cnf="intermediate/openssl.cnf"
+  echo "[ req ]
+default_bits = 2048
+prompt = no
+encrypt_key = no
+default_md = sha256
+distinguished_name = dn
+utf8 = yes
+
+[ dn ]
+C = NO
+O = My Org
+CN = ${host}
+" > cnf/${host}.cnf
+  cnf="cnf/${host}.cnf"
 fi
 
 echo "Using ${cnf} as config file"
 
 openssl genrsa -out intermediate/private/${host}.key.pem 2048
 
-
-openssl req -config ${cnf} -key intermediate/private/${host}.key.pem -new -sha256 -out intermediate/csr/${host}.csr.pem
+openssl req -config ${cnf} -batch -key intermediate/private/${host}.key.pem -new -sha256 -out intermediate/csr/${host}.csr.pem
 
 openssl ca -config intermediate/openssl.cnf -extensions v3_req \
   -days 375 -notext -md sha256 -in intermediate/csr/${host}.csr.pem \
